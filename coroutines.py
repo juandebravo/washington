a2a42415edb4291dabb6d919f17f2848c380c4af
juandebravo <juandebravo@gmail.com>
2015-04-24 02:31:04 -0400

    intermediate state with method + execute

--- coroutines.py
+++ coroutines.py
@@ -1,4 +1,5 @@
 import re
+from functools import partial
 
 
 def coroutine(func):
@@ -49,30 +50,63 @@ def pluck(field, destination):
         destination.send((yield)[field])
 
 
-def pipeline(employees, destination):
-    def execute():
-        for e in employees:
-            destination.send(e)
-        destination.close()
+def pipeline(employees):
 
-    return execute
+    class Method(object):
+
+        def __init__(self, method, execute):
+            self.execute = execute
+            self.fname = globals()[method]
+
+        def __call__(self, *args):
+            self.fn = partial(self.fname, *args)
+            self.execute.stack.append(self)
+            return self.execute
+
+        def send(self, i):
+            self.fn(self.execute.stack[i+1].fn)
+
+    class Execute(object):
+
+        def __init__(self, employees):
+            self.stack = []
+            self.employees = employees
+
+        def __call__(self):
+            for e in self.employees:
+                self.stack[0].send(e)
+            self.stack[0].close()
+
+        def __getattr__(self, value):
+            if value in ('create_dict', 'filter_by', 'pluck', 'print_name'):
+                return Method(value, self)
+            else:
+                return self.__dict__.get(value)
+
+    return Execute(employees)
 
 
 employees = open('file.txt')
 
-alias_contains_u_not_at_the_beginning = pipeline(
-    employees,
-    create_dict(
-        ('alias', 'email', 'location'),
-        filter_by(
-            'alias',
-            r'^[^u]+u',
-            pluck(
-                'alias',
-                print_name('result.log')
-            )
-        )
-    )
-)
+# alias_contains_u_not_at_the_beginning = pipeline(
+#     employees,
+#     create_dict(
+#         ('alias', 'email', 'location'),
+#         filter_by(
+#             'alias',
+#             r'^[^u]+u',
+#             pluck(
+#                 'alias',
+#                 print_name('result.log')
+#             )
+#         )
+#     )
+# )
+
+alias_contains_u_not_at_the_beginning = (pipeline(employees).
+                                         create_dict(('alias', 'email', 'location')).
+                                         filter_by('alias', r'^[^u]+u').
+                                         pluck('alias').
+                                         print_name('result.log'))
 
 alias_contains_u_not_at_the_beginning()
