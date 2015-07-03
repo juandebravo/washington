f36bb0f2543efda756bdf987a215624df823d1b2
juandebravo <juandebravo@gmail.com>
2015-07-04 00:33:39 +0200

    first pipeline unit tests

--- tests/main_tests.py
+++ tests/main_tests.py
@@ -1,13 +1,35 @@
 import unittest
 import logging
+from nose.plugins.skip import SkipTest
+
 import os
 
-from pyshould import should
+from pyshould import should, all_of
 
 from StringIO import StringIO
 from types import GeneratorType
 
-from washington.main import coroutine
+from washington import register
+from washington.main import coroutine, pipeline
+
+
+@coroutine
+def uppercase(destination=None):
+    while True:
+        value = yield
+        if destination is not None:
+            destination.send(str(value).upper())
+
+
+@coroutine
+def collect(where, destination=None):
+    while True:
+        value = yield
+        where.append(value)
+        if destination is not None:
+            destination.send(str(value).upper())
+
+register(uppercase, collect)
 
 
 class CorouteTestCase(unittest.TestCase):
@@ -52,3 +74,24 @@ class CorouteTestCase(unittest.TestCase):
         self.handler.flush()
         self.stream.getvalue() | should.eql(str(start) + os.linesep)
 
+
+class PipelineTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.recollector = []
+        self.pipeline = (
+            pipeline((x for x in ('foo', 'bar', 'bazz')))
+            .uppercase()
+            .collect(self.recollector)
+        )
+
+    def test_chains_coroutines(self):
+        self.pipeline()
+        all_of(('FOO', 'BAR', 'BAZZ')) | should.be_in(self.recollector)
+        len(self.recollector) | should.be(3)
+
+    def test_raises_exception_if_invalid_coroutine(self):
+        raise SkipTest
+
+    def test_exposes_a_valid_stack_trace(self):
+        raise SkipTest
--- washington/__init__.py
+++ washington/__init__.py
@@ -1,1 +1,2 @@
-from main import *
+from .main import *
+from .register import register
